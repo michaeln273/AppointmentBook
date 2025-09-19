a3a911683739a4902767aa74c16d8a54e732aa7b
MichaelNikoloudakis <michaeln273@nycstudents.net>
2025-09-19 13:12:32 -0400

    d

---
+++ src/AppointmentBook.java
@@ -0,0 +1,26 @@
+public class AppointmentBook {
+    private boolean[][] schedule;
+
+    public AppointmentBook(boolean[][] schedule)
+    {
+        this.schedule = schedule;
+    }
+
+    private boolean isMinuteFree(int period, int minute)
+    {
+        return false;
+    }
+
+    private void reserveBlock(int period, int startMinute, int duration) {
+    }
+
+    public int findFreeBlock(int period, int duration)
+    {
+        return 0;
+    }
+
+    public boolean makeAppointment(int startPeriod, int endPeriod, int duration)
+    {
+        return false;
+    }
+}
